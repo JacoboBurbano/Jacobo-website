a8b90b06fbedca2f7cf0f9963e00e42ac81879bd
Jacobo <jacoboburbano2004@gmail.com>
2024-10-17 17:17:41 -0500

    Try to refactor the Navbar component or just make work the responsive

--- src/navbar/Navbar.tsx
+++ src/navbar/Navbar.tsx
@@ -1,14 +1,29 @@
 import './Navbar.css'
 import NavImage from '../images/nav.png'
 import { InterfaceItem } from './interface-navbar'
+import { useEffect, useRef } from 'react'
+import { LegacyRef } from 'react'
 export const NavBar = () => {
     const itemsNavbar: InterfaceItem[] = [{name: 'Home', path: '.'}, {name: 'Projects', path: '.'}, {name: 'Skills', path: '.'}]
-    return ( 
+    let imgNav: null | LegacyRef<HTMLImageElement> = useRef(null)
+    useEffect(() => {
+        if(imgNav.current){
+            imgNav.current.addEventListener('click', (event: Event) => {
+                console.log(event.target)
+            })
+        }
+    },[])
+    return (
         <nav className="navbar">
+            <ul className="navbar-list__responsive">
+                <img src={NavImage} alt="" className='nav-image' ref={imgNav}/>
+                {itemsNavbar.map((item, index) => (
+                    <Item name={item.name} path={item.path} key={index}/>
+                ))}
+            </ul>
             <ul className="navbar-list">
-            <img src={NavImage} alt="" className='nav-image' />
-                {itemsNavbar.map(item => (
-                    <Item name={item.name} path={item.path} />
+                {itemsNavbar.map((item, index) => (
+                    <Item name={item.name} path={item.path} key={index}/>
                 ))}
             </ul>
         </nav>
