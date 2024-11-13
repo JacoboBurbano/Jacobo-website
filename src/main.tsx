8b2e3d0580f45fb20c62c593c586ab72959cd7ad
Jacobo <jacoboburbano2004@gmail.com>
2024-11-13 11:36:39 -0500

    improving the page and new info added

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
-import React, { createContext } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {Info} from './Jacob-info.tsx'
 import './index.css'
 import {
   createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
+  RouterProvider
 } from "react-router-dom";
 import { SkillsPage } from './skills/Skills.tsx';
 import { ProjectsPage } from './projects/Projects.tsx';
